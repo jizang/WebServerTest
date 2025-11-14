39ed012b60ac2ad3c69d7c28c1bfd4c3eeb2644a
jizang <jizang.lu@gmail.com>
2025-11-14 13:49:33 +0800

    4. 實作：側邊導航 (Sidebar) 4-1. 建立 ViewModel 4-2. 建立元件類別 4-3. 建立元件視圖 4-4. 整合至 Layout

---
+++ WebServer/Components/SidebarComponent.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebServer.Models.ViewModels;
+
+namespace WebServer.Components;
+
+[ViewComponent(Name = "Sidebar")]
+public class SidebarComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        await Task.Yield();
+
+        // 定義選單結構
+        var model = new SidebarViewModel
+        {
+            MenuItems = new List<MenuItem>
+            {
+                // 1. 首頁
+                new MenuItem
+                {
+                    Title = "儀表板",
+                    Controller = "Home",
+                    Action = "Index",
+                    URL = "/Home/Index",
+                    Icon = "menu-icon tf-icons bx bx-home-circle",
+                },
+
+                // 2. 系統管理 (結合之前的 User 功能)
+                new MenuItem
+                {
+                    Title = "系統管理",
+                    URL = "javascript:void(0);",
+                    Icon = "menu-icon tf-icons bx bx-cog",
+                    SubItems = new List<MenuItem>
+                    {
+                        new MenuItem {
+                            Title = "使用者帳號",
+                            Controller = "User",
+                            Action = "Index",
+                            URL = "/User/Index",
+                        },
+                        new MenuItem {
+                            Title = "檔案管理",
+                            Controller = "FileStorage",
+                            Action = "Index",
+                            URL = "/FileStorage/Index",
+                        }
+                    }
+                },
+
+                // 3. 業務管理 (結合北風資料庫) - 這裡先預留連結，後續課程實作 Controller
+                new MenuItem
+                {
+                    Title = "業務管理",
+                    URL = "javascript:void(0);",
+                    Icon = "menu-icon tf-icons bx bx-cart", // 購物車圖示
+                    SubItems = new List<MenuItem>
+                    {
+                        new MenuItem {
+                            Title = "產品列表",
+                            Controller = "Product",
+                            Action = "Index",
+                            URL = "/Product/Index",
+                        },
+                        new MenuItem {
+                            Title = "訂單檢視",
+                            Controller = "Order",
+                            Action = "Index",
+                            URL = "/Order/Index",
+                        },
+                        new MenuItem {
+                            Title = "客戶資料",
+                            Controller = "Customer",
+                            Action = "Index",
+                            URL = "/Customer/Index",
+                        }
+                    }
+                },
+
+                // 4. 個人設定 (User Settings)
+                new MenuItem
+                {
+                    Title = "個人設定",
+                    URL = "javascript:void(0);",
+                    Icon = "menu-icon tf-icons bx bx-user",
+                    SubItems = new List<MenuItem>
+                    {
+                        new MenuItem {
+                            Title = "變更密碼",
+                            Controller = "Account",
+                            Action = "ChangePassword",
+                            URL = "/Account/ChangePassword"
+                        },
+                        new MenuItem {
+                            Title = "登出",
+                            URL = "/Account/Signout"
+                        },
+                    }
+                }
+            }
+        };
+
+        return View("Default", model);
+    }
+}
---
+++ WebServer/Models/ViewModels/SidebarViewModel.cs
@@ -0,0 +1,52 @@
+﻿namespace WebServer.Models.ViewModels;
+
+/// <summary>
+/// 用於呈現網站側邊導覽列 (Sidebar) 的視圖模型。
+/// </summary>
+public class SidebarViewModel
+{
+    /// <summary>
+    /// 取得或設定頂層選單項目的集合。
+    /// </summary>
+    public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
+}
+
+/// <summary>
+/// 代表導覽選單中的單一項目，支援階層式結構。
+/// </summary>
+public class MenuItem
+{
+    /// <summary>
+    /// 取得或設定選單項目的顯示標題。
+    /// </summary>
+    public string Title { get; set; } = null!;
+
+    /// <summary>
+    /// 取得或設定對應的 ASP.NET Core 控制器 (Controller) 名稱。
+    /// <para>若有設定此值，將優先用於產生連結與判斷 Active 狀態。</para>
+    /// </summary>
+    public string? Controller { get; set; }
+
+    /// <summary>
+    /// 取得或設定對應的動作 (Action) 名稱。
+    /// <para>若有設定此值，將優先用於產生連結與判斷 Active 狀態。</para>
+    /// </summary>
+    public string? Action { get; set; }
+
+    /// <summary>
+    /// 取得或設定選單項目的目標連結網址。
+    /// <para>若已設定 Controller 與 Action，此欄位可忽略；若此項目僅作為展開子選單的容器，此值可能為 null。</para>
+    /// </summary>
+    public string? URL { get; set; }
+
+    /// <summary>
+    /// 取得或設定選單項目的圖示 (例如：FontAwesome 或 Bootstrap Icons 的 CSS 類別名稱)。
+    /// </summary>
+    public string? Icon { get; set; }
+
+    /// <summary>
+    /// 取得或設定子選單項目集合 (支援遞迴結構)。
+    /// <para>若沒有子項目，則為空清單。</para>
+    /// </summary>
+    public List<MenuItem> SubItems { get; set; } = new List<MenuItem>();
+}
